8796583db1fc884ed05e2765c5ee425acf416efb
tanduy <nguyentanduy080504@gmail.com>
2025-09-18 12:52:05 +0700

    Add client and server source code

---
+++ server/src/main/java/com/example/SendFile/SendFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.SendFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendFileApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/SendFile/SendFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SendFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
